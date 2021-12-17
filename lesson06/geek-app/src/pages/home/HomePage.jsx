ea4d8fec0ad926fd0c7cb601b97ca928e99fc6fd
Valeriy 7 <you@example.com>
2021-12-17 17:44:59 +0400

    add homework6

---
+++ lesson06/geek-app/src/pages/home/HomePage.jsx
@@ -0,0 +1,12 @@
+import styles from './Home.module.css';
+
+const HomePage = () => {
+    //
+    return (
+        <div className={styles.MyHome}>
+            <p>Learning React</p>
+        </div>
+    );
+}
+
+export { HomePage };
